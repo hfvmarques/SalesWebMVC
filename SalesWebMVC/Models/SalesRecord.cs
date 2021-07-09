4248c253d0cde15cc7a940b0db1126837b134a19
Henrique Marques <hfvmarques@gmail.com>
2021-07-08 22:04:53 -0400

    Correction applied on SalesRecord Model

--- SalesWebMVC/Models/SalesRecord.cs
+++ SalesWebMVC/Models/SalesRecord.cs
@@ -9,16 +9,16 @@ namespace SalesWebMVC.Models
         public DateTime Date { get; set; }
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
-        public Seller MyProperty { get; set; }
+        public Seller Seller { get; set; }
 
         public SalesRecord() { }
-                public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller myProperty)
+                public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
         {
             Id = id;
             Date = date;
             Amount = amount;
             Status = status;
-            MyProperty = myProperty;
+            Seller = seller;
         }
     }
 }
